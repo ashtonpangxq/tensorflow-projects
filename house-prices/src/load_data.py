57c87233fba6da4584f9cbc4539a52c9338d2117
Ashton Pang <qun.pang@gmail.com>
2022-07-15 00:15:39 +0800

    Updated load_data.py module

--- house-prices/src/load_data.py
+++ house-prices/src/load_data.py
@@ -12,18 +12,3 @@ def get_data(train_path: str, test_path: str) -> Tuple[pd.DataFrame, pd.DataFram
     test = pd.read_csv(test_data_path)
 
     return train, test
-
-
-def get_combined_data(
-    train_df: pd.DataFrame, test_df: pd.DataFrame
-) -> Tuple[pd.DataFrame, pd.Series]:
-
-    target = train_df.SalePrice
-    train_df.drop(["SalePrice"], axis=1, inplace=True)
-
-    combined = train_df.append(test_df)
-    combined.reset_index(inplace=True)
-    combined.drop(["index", "Id"], inplace=True, axis=1)
-
-    return combined, target
-
